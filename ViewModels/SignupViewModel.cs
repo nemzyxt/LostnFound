879324c1e37a207a7fd2f73403b4d65f70e302fb
Nemuel Wainaina <bR0S3@protonmail.com>
2023-11-28 05:38:33 +0300

    Define login and signup view models

---
+++ ViewModels/SignupViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LostnFound.ViewModels 
+{
+    public class SignupViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
