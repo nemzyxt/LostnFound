c73574aaea5a2495ba4d92d914a9dbd44da4ecb3
Nemuel Wainaina <bR0S3@protonmail.com>
2023-11-28 07:55:35 +0300

    Update to check whether or not UserId is null

--- Controllers/LostItemController.cs
+++ Controllers/LostItemController.cs
@@ -36,11 +36,25 @@ public class LostItemController : Controller
     }
 
     // Action to return details of a specific book by id
-    public IActionResult MyItems(int id)
+    public IActionResult MyItems()
     {
-        // Get lost items for user with the provided id
-        var myItems = _lostItemService.GetItemsForUser(id);
+        // Retrieve the user id from SessionStorage
+        var userId = HttpContext.Session.GetInt32("UserId");
 
+        IEnumerable<LostItemModel> myItems = null;
+
+        if (userId.HasValue)
+        {
+            myItems = _lostItemService.GetItemsForUser(userId.Value);
+        }
+        else
+        {
+            // Handle the case where userId is null (if needed)
+            ModelState.AddModelError(string.Empty, "User ID is null");
+            return RedirectToAction("Login", "Auth");
+        }
+
+        // Pass the list of items to the view
         return View(myItems);
     }
 }
