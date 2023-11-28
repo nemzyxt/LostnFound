6fac0ff329f298298d46e7586adeeb1586b53cb9
Nemuel Wainaina <bR0S3@protonmail.com>
2023-11-28 06:06:04 +0300

    Finish up on Landing page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LostnFound.Models;
@@ -19,11 +15,6 @@ namespace LostnFound.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
