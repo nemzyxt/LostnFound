cbde013c5b312d3228ed82866b43c8ec004b2012
Nemuel Wainaina <bR0S3@protonmail.com>
2023-11-28 06:37:49 +0300

    Define the LostItemModel

--- Models/LostItemModel.cs
+++ Models/LostItemModel.cs
@@ -1,11 +1,19 @@
+using System;
+
 namespace LostnFound.Models
 {
     public class LostItemModel
     {
-        // primary key
-        public int UserId { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; } // Store hashed passwords
-        public string Email { get; set; }
+        public int ItemId { get; set; }
+        public int OwnerId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public DateTime DateLost { get; set; }
+        public string LocationLost { get; set; }
+        public bool IsClaimed { get; set; }
+        public int FinderId { get; set; }
+        public DateTime DateFound { get; set; }
+        public string LocationFound { get; set; }
+        public string FinderContact { get; set; }
     }
 }
