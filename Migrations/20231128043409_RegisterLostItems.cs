597af79672997638d01829428e4fb4ad4ddbaf95
Nemuel Wainaina <bR0S3@protonmail.com>
2023-11-28 07:35:55 +0300

    Make migrations

---
+++ Migrations/20231128043409_RegisterLostItems.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LostnFound.Migrations
+{
+    public partial class RegisterLostItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LostItemModel",
+                table: "LostItemModel");
+
+            migrationBuilder.RenameTable(
+                name: "LostItemModel",
+                newName: "LostItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LostItems",
+                table: "LostItems",
+                column: "ItemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LostItems",
+                table: "LostItems");
+
+            migrationBuilder.RenameTable(
+                name: "LostItems",
+                newName: "LostItemModel");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LostItemModel",
+                table: "LostItemModel",
+                column: "ItemId");
+        }
+    }
+}
