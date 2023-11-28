6e52ec3d83e3ab61547738b5dd5f0b0853d01535
Nemuel Wainaina <bR0S3@protonmail.com>
2023-11-28 05:35:51 +0300

    Define the database context

---
+++ Models/LostnFoundContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LostnFound.Models
+{
+    public class LostnFoundContext : DbContext
+    {
+        public LostnFoundContext(DbContextOptions<LostnFoundContext> options) : base(options)
+        {
+        }
+
+        // DbSet properties represent database tables
+        public DbSet<AuthModel> Auth { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuthModel>().HasKey(e => e.UserId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
